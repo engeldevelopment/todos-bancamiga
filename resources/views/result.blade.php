3761ed4f804d89cf14f9d534be1c0be07024bb6b
Jesus Garcia <BA0669@bancamiga.com>
2021-10-30 15:06:13 -0400

    style: Maquetando.

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    {{ $result }}
+    
+<div class="row">
+    <div class="col-12">
+        <article class="card">
+            <div class="card-body">
+              <h4> El resultado es:  <strong class="text-success">{{ $result }} </strong> </h4> 
+            </div>
+        </article>
+    </div>
+</div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,13 @@
 <div>
     <div class="row">
         <div class="col-12">
-
             <form action="{{ route("sumar") }}" method="POST">
                 @csrf
                 <input type="number"  name="a" class="form-control">
                 <input type="number" name="b" class="form-control">
                 <button type="submit" class="btn btn-primary">Guardar</button>
             </form>
-            
         </div>
     </div>
-    
 @endsection
    
